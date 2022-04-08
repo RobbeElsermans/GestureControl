e61fb245147363ae2fd862d6228398294d28e0b1
Robbe Elsermans <robels@outlook.com>
2022-04-08 08:57:59 +0200

    werking all ToF interrupt

--- Code PlatformIO/Gesture Control PCBV0.3/src/app_tof.c
+++ Code PlatformIO/Gesture Control PCBV0.3/src/app_tof.c
@@ -203,9 +203,6 @@ extern "C"
 		// // setXTalkCompensation(VL53L3A2_DEV_BOTTOM, 1);
 	}
 
-	/*
-	 * LM background task
-	 */
 	void MX_TOF_Process(void *_htim3, void *_hi2c3)
 	{
 		// htim3 = *(TIM_HandleTypeDef *)_htim3;
@@ -216,240 +213,31 @@ extern "C"
 
 		HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, 1);
 		start_sensor(VL53L3A2_DEV_CENTER);
-		HAL_Delay(2000);
+		start_sensor(VL53L3A2_DEV_LEFT);
+		start_sensor(VL53L3A2_DEV_RIGHT);
+		start_sensor(VL53L3A2_DEV_TOP);
+		start_sensor(VL53L3A2_DEV_BOTTOM);
+		
+		//HAL_Delay(2000);
 
-		int8_t sta1 = 0;
+		int8_t sta4 = 0;
 
 		while(1){
-			HAL_Delay(20);
-			getResult(VL53L3A2_DEV_CENTER, Result);
-			HAL_Delay(20);
-			dis1 = getDistance(VL53L3A2_DEV_CENTER, Result, dis1);
-			sta1 = (int)Result[VL53L3A2_DEV_CENTER].ZoneResult[0].Status[0];
-			HAL_Delay(500);
+			//getResult(VL53L3A2_DEV_CENTER, Result);
+			//HAL_Delay(20);
+			checkSensorReady(&isStarted0, &isReady0, VL53L3A2_DEV_CENTER, Result);
+			checkSensorReady(&isStarted1, &isReady1, VL53L3A2_DEV_LEFT, Result);
+			checkSensorReady(&isStarted2, &isReady2, VL53L3A2_DEV_RIGHT, Result);
+			checkSensorReady(&isStarted3, &isReady3, VL53L3A2_DEV_TOP, Result);
+			checkSensorReady(&isStarted4, &isReady4, VL53L3A2_DEV_BOTTOM, Result);
+			//HAL_Delay(20);
+			dis4 = (long)Result[VL53L3A2_DEV_BOTTOM].ZoneResult[0].Distance[0];
+			sta4 = (int)Result[VL53L3A2_DEV_BOTTOM].ZoneResult[0].Status[0];
+			//HAL_Delay(500);
 			HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
-			printf("dis: %4d, stat: %2d \r\n",dis1,sta1);
+			printf("dis: %4d, stat: %2d \r\n",dis4,sta4);
+			HAL_Delay(200);
 		}
-
-		// while (1)
-		// {
-		// 	HAL_Delay(20);
-		// 	timerTicks = HAL_GetTick();
-		// 	getResult(VL53L3A2_DEV_CENTER, Result);
-
-		// 	// checkSensorReady(&isStarted1, &isReady1, VL53L3A2_DEV_CENTER, Result);
-
-		// 	dis1 = getDistance(VL53L3A2_DEV_CENTER, Result, dis1);
-		// 	dis0 = getDistance(VL53L3A2_DEV_LEFT, Result, dis0);
-		// 	dis2 = getDistance(VL53L3A2_DEV_RIGHT, Result, dis2);
-		// 	dis3 = getDistance(VL53L3A2_DEV_TOP, Result, dis3);
-		// 	dis4 = getDistance(VL53L3A2_DEV_BOTTOM, Result, dis4);
-
-		// 	// objectPresent = ckeckObjectPresent(Result, &objectPresent, &dis1);
-
-		// 	if (objectPresent)
-		// 	{
-		// 		if (!hasStarted) // Start andere sensoren ook op
-		// 		{
-		// 			hasStarted = true;
-		// 			start_sensor(VL53L3A2_DEV_LEFT);
-		// 			start_sensor(VL53L3A2_DEV_RIGHT);
-
-		// 			start_sensor(VL53L3A2_DEV_TOP);
-		// 			start_sensor(VL53L3A2_DEV_BOTTOM);
-
-		// 			// PWM timer starten
-		// 			// HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
-		// 		}
-
-		// 		// Resultaten opvragen sensoren
-		// 		checkSensorReady(&isStarted0, &isReady0, VL53L3A2_DEV_LEFT, Result);
-		// 		dis0 = getDistance(VL53L3A2_DEV_LEFT, Result, dis0);
-
-		// 		// Resultaten opvragen sensoren
-		// 		checkSensorReady(&isStarted2, &isReady2, VL53L3A2_DEV_RIGHT, Result);
-		// 		dis2 = getDistance(VL53L3A2_DEV_RIGHT, Result, dis2);
-
-		// 		checkSensorReady(&isStarted3, &isReady3, VL53L3A2_DEV_TOP, Result);
-		// 		dis3 = getDistance(VL53L3A2_DEV_TOP, Result, dis3);
-
-		// 		checkSensorReady(&isStarted4, &isReady4, VL53L3A2_DEV_BOTTOM, Result);
-		// 		dis4 = getDistance(VL53L3A2_DEV_BOTTOM, Result, dis4);
-		// 	}
-		// 	else
-		// 	{
-		// 		if (hasStarted)
-		// 		{
-		// 			hasStarted = false;
-		// 			stop_sensor(VL53L3A2_DEV_LEFT);
-		// 			stop_sensor(VL53L3A2_DEV_RIGHT);
-
-		// 			stop_sensor(VL53L3A2_DEV_TOP);
-		// 			stop_sensor(VL53L3A2_DEV_BOTTOM);
-
-		// 			// PWM timer starten
-		// 			// HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_2);
-
-		// 			// HAL_Delay(2);
-		// 		}
-		// 	}
-
-		// 	// if (objectPresent)
-		// 	//__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, (pwmVal * 2) + 23);
-
-		// 	int sta0 = (int)Result[VL53L3A2_DEV_LEFT].ZoneResult[0].Status[0];
-		// 	int sta1 = (int)Result[VL53L3A2_DEV_CENTER].ZoneResult[0].Status[0];
-		// 	int sta2 = (int)Result[VL53L3A2_DEV_RIGHT].ZoneResult[0].Status[0];
-		// 	int sta3 = (int)Result[VL53L3A2_DEV_TOP].ZoneResult[0].Status[0];
-		// 	int sta4 = (int)Result[VL53L3A2_DEV_BOTTOM].ZoneResult[0].Status[0];
-
-		// 	int obj0 = (int)Result[VL53L3A2_DEV_LEFT].ZoneResult[0].NumberOfTargets;
-		// 	int obj1 = (int)Result[VL53L3A2_DEV_CENTER].ZoneResult[0].NumberOfTargets;
-		// 	int obj2 = (int)Result[VL53L3A2_DEV_RIGHT].ZoneResult[0].NumberOfTargets;
-		// 	int obj3 = (int)Result[VL53L3A2_DEV_TOP].ZoneResult[0].NumberOfTargets;
-		// 	int obj4 = (int)Result[VL53L3A2_DEV_BOTTOM].ZoneResult[0].NumberOfTargets;
-
-		// 	printf("left: %5d obj: %1d sta: %2d \t center: %5d obj: %1d sta: %2d \t right: %5d obj: %d sta: %2d \t top: %5d obj: %d sta: %2d \t bottom: %5d obj: %d sta: %2d", dis0, obj0, sta0, dis1, obj1, sta1, dis2, obj2, sta2, dis3, obj3, sta3, dis4, obj4, sta4);
-		// 	printf("\r\n");
-
-		// 	// printf("dist1: %5d \r\n", dis1);
-
-		// 	// Kijken ofdat er een dimming commando aanwezig is
-		// 	gestureDimming = CheckDimmingCommand(&gestureDimming, &objectPresent, &dis1, &sta1);
-
-		// 	// Wanneer er geen dimming commando aanwezig is dan kijken we of dat er een Right Left beweging aanwezig is
-		// 	if (!gestureDimming)
-		// 		gestureRL = CheckGestureRL(&gestureRL, &objectPresent, Result);
-
-		// 	// Wanneer er geen dimming commando aanwezig is dan kijken we of dat er een Left Right beweging aanwezig is
-		// 	if (!gestureDimming)
-		// 		gestureLR = CheckGestureLR(&gestureLR, &objectPresent, Result);
-
-		// 	// Wanneer er geen dimming commando aanwezig is dan kijken we of dat er een Down Up beweging aanwezig is
-		// 	if (!gestureDimming)
-		// 		gestureDU = CheckGestureDU(&gestureDU, &objectPresent, Result);
-
-		// 	// Wanneer er geen dimming commando aanwezig is dan kijken we of dat er een Down Up beweging aanwezig is
-		// 	if (!gestureDimming)
-		// 		gestureUD = CheckGestureUD(&gestureUD, &objectPresent, Result);
-
-		// 	// Het commando dimming activeren
-		// 	if (gestureDimming && (commando == NONE || commando == DIM))
-		// 	{
-		// 		commando = DIM;
-		// 		pwmVal = getDimmingValue(&gestureDimming, &pwmVal, &dis1);
-		// 	}
-
-		// 	// Het commando RL activeren
-		// 	if (gestureRL && !prevGestureRL && commando == NONE)
-		// 	{
-		// 		prevGestureRL = gestureRL;
-		// 		printf("gestureCommand RL: %d \r\n", gestureRL);
-		// 		commando = RL;
-		// 	}
-
-		// 	// Het commando LR activeren
-		// 	if (gestureLR && !prevGestureLR && commando == NONE)
-		// 	{
-		// 		prevGestureLR = gestureLR;
-		// 		printf("gestureCommand LR: %d \r\n", gestureLR);
-		// 		commando = LR;
-		// 	}
-
-		// 	// Het commando DU activeren
-		// 	if (gestureDU && !prevGestureDU && commando == NONE)
-		// 	{
-		// 		prevGestureDU = gestureDU;
-		// 		printf("gestureCommand DU: %d \r\n", gestureDU);
-		// 		commando = DU;
-		// 	}
-
-		// 	// Het commando UD activeren
-		// 	if (gestureUD && !prevGestureUD && commando == NONE)
-		// 	{
-		// 		prevGestureUD = gestureUD;
-		// 		printf("gestureCommand UD: %d \r\n", gestureUD);
-		// 		commando = UD;
-		// 	}
-
-		// 	prevGestureRL = gestureRL; // fix debouncing van gestureRL
-		// 	prevGestureLR = gestureLR; // fix debouncing van gestureLR
-		// 	prevGestureDU = gestureDU; // fix debouncing van gestureDU
-		// 	prevGestureUD = gestureUD; // fix debouncing van gestureUD
-
-		// 	// Refreshrate van schakeling zichtbaar maken
-		// 	HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
-
-		// 	/* 	Timer om leds even aan te laten
-		// 		Er wordt gekeken wanneer commando veranderd wordt naar alles behalve NONE.
-		// 		Dan zetten we een timer
-		// 		Wanneer de timer afloopt wordt het commando gereset
-		// 	*/
-		// 	if (!timerCommandSet && commando != NONE)
-		// 	{
-		// 		timerCommandSet = true;
-		// 		timerCommand = HAL_GetTick();
-		// 	}
-		// 	if ((HAL_GetTick() - timerCommand) >= timerCommandTimeout)
-		// 	{
-		// 		timerCommandSet = false;
-		// 		commando = NONE;
-		// 	}
-		// 	// Commando's uitsturen
-		// 	switch (commando)
-		// 	{
-		// 	case NONE:
-		// 		HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_RESET);
-		// 		HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_RESET);
-		// 		HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_RESET);
-		// 		break;
-		// 	case RL:
-		// 		HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
-		// 		HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_RESET);
-		// 		HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_RESET);
-		// 		break;
-		// 	case LR:
-		// 		HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
-		// 		HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_SET);
-		// 		HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_RESET);
-		// 		break;
-		// 	case UD:
-		// 		HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_RESET);
-		// 		HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_RESET);
-		// 		HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_SET);
-		// 		break;
-		// 	case DU:
-		// 		HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
-		// 		HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_RESET);
-		// 		HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_SET);
-		// 		break;
-		// 	case DIM:
-		// 		HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_RESET);
-		// 		HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_SET);
-		// 		HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_SET);
-		// 		break;
-
-		// 	default:
-		// 		HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_RESET);
-		// 		HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_RESET);
-		// 		HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_RESET);
-		// 		break;
-		// 	}
-		// 	HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, objectPresent);
-
-		// 	// I2C Slave spelen zonder delay
-		// 	HAL_I2C_Slave_Receive_IT(&hi2c3, &buf, sizeof(buf));
-
-		// 	HAL_Delay(10);
-		// 	if (timerTicksCount == 0)
-		// 	{
-		// 		timeTicks = 0;
-		// 		timeTicks = (HAL_GetTick() - timerTicks);
-		// 		timerTicks = 0;
-		// 		printf("time: %5d ms \r\n", timeTicks);
-		// 	}
-		// 	timerTicksCount++;
-		// }
 	}
 
 	static void MX_53L3A2_MultiSensorRanging_Init(void)
@@ -508,8 +296,8 @@ extern "C"
 		Profile.EnableSignal = 1;	/* Enable: 1, Disable: 0 */
 
 		VL53L3A2_RANGING_SENSOR_ConfigProfile(sensor, &Profile);
-		// status = VL53L3A2_RANGING_SENSOR_Start(sensor, VL53L3CX_MODE_ASYNC_CONTINUOUS);
-		status = VL53L3A2_RANGING_SENSOR_Start(sensor, VL53L3CX_MODE_BLOCKING_CONTINUOUS);
+		status = VL53L3A2_RANGING_SENSOR_Start(sensor, VL53L3CX_MODE_ASYNC_CONTINUOUS);
+		// status = VL53L3A2_RANGING_SENSOR_Start(sensor, VL53L3CX_MODE_BLOCKING_CONTINUOUS);
 
 		if (status != BSP_ERROR_NONE)
 			while (1)
@@ -530,11 +318,13 @@ extern "C"
 		{
 			*_isReady = false;
 			getResult(sensor, result);
+			//printf("getResult \r\n");
 		}
 		else if (!*_hasStarted)
 		{
 			*_hasStarted = true;
 			getResult(sensor, result);
+			//printf("trash \r\n");
 		}
 	}
 
